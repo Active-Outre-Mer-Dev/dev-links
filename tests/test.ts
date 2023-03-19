55aa9d497a517fd9e5eea6299e076b5ff7ee070e
bluepnwage <a.carty2555@gmail.com>
2023-03-18 21:36:48 -0400

    create tests for links

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,24 @@
 import { expect, test } from "@playwright/test";
+import links from "../src/lib/links.json" assert { type: "json" };
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("https://dev-links-lemon.vercel.app/");
+});
 
 test("home page has expected title", async ({ page }) => {
-  await page.goto("https://dev-links-lemon.vercel.app/");
   expect(await page.title()).toBe("DevLinks");
+});
+
+test("Links are visible", async ({ page }) => {
+  const promises = links.tech.map(async link => {
+    return expect(page.getByRole("link", { name: link.label })).toBeVisible();
+  });
+  await Promise.all(promises);
+});
+
+test("Links have correct href", async ({ page }) => {
+  const promises = links.tech.map(async link => {
+    return expect(await page.getByRole("link", { name: link.label }).getAttribute("href")).toBe(link.href);
+  });
+  await Promise.all(promises);
 });
