760e4235d949c086a1008786498ec1e861efb277
bluepnwage <a.carty2555@gmail.com>
2023-03-18 20:46:33 -0400

    fix test

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from "@playwright/test";
 
 test("home page has expected title", async ({ page }) => {
-  await page.goto("/");
+  await page.goto("https://dev-links-lemon.vercel.app/");
   expect(await page.title()).toBe("DevLinks");
 });
