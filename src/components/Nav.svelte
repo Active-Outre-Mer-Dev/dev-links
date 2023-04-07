06505b4c4515a6404c256a1c31ed5bb03186f604
bluepnwage <a.carty2555@gmail.com>
2023-04-06 23:54:29 -0400

    create navbar

---
+++ src/components/Nav.svelte
@@ -0,0 +1,76 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  let AlgoliaSearch: any;
+
+  let showSearch = false;
+  let init = false;
+  onMount(() => {
+    const focus = (e: KeyboardEvent) => {
+      if (e.ctrlKey && e.key === "k") {
+        e.preventDefault();
+        showSearch = !showSearch;
+        if (!init) init = true;
+      }
+    };
+    window.addEventListener("keydown", focus);
+    return () => window.removeEventListener("keydown", focus);
+  });
+  const onClose = () => {
+    showSearch = false;
+  };
+  const onOpen = () => {
+    showSearch = true;
+    if (!init) init = true;
+  };
+  const dynamicImport = async () => {
+    AlgoliaSearch = (await import("./AlgoliaSearch.svelte")).default;
+  };
+  $: {
+    if (init) {
+      dynamicImport();
+    }
+  }
+</script>
+
+<header
+  class="border-b flex items-center justify-center border-neutral-700 bg-neutral-800/50 h-16 w-full sticky top-0 left-0 backdrop-blur-sm"
+>
+  <div class="w-4/5 justify-between flex items-center h-full">
+    <div>
+      <img src="/aom.svg" alt="" width={65} height={65} />
+    </div>
+    <button
+      on:click={onOpen}
+      class={`rounded-full h-3/4 px-8 gap-8  flex items-center justify-between
+       bg-neutral-900 border border-neutral-700`}
+    >
+      <div class="space-x-2 text-neutral-400">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="inline-block"
+          width="16"
+          height="16"
+          viewBox="0 0 24 24"
+          stroke-width="2"
+          stroke="currentColor"
+          fill="none"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
+          <path d="M21 21l-6 -6" />
+        </svg>
+        <span> Search </span>
+      </div>
+      <div class="text-neutral-300">
+        <kbd class="bg-neutral-800 p-1 w-5 h-5 text-xs rounded-sm uppercase">Ctrl</kbd>
+        <kbd class="bg-neutral-800 p-1 w-5 h-5 text-xs rounded-sm">K</kbd>
+      </div>
+    </button>
+  </div>
+</header>
+{#if showSearch}
+  <svelte:component this={AlgoliaSearch} {onClose} />
+{/if}
