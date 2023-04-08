c55a1426660f7e7a77b2672cb6ba61bb2e5d1f44
bluepnwage <a.carty2555@gmail.com>
2023-04-07 20:34:15 -0400

    create global 404 page

--- src/routes/[category]/+layout.ts
+++ src/routes/[category]/+layout.ts
@@ -1,9 +1,13 @@
 import json from "$lib/links.json";
-
+import { error } from "@sveltejs/kit";
 type Categories = keyof typeof json;
 
 export function load(event) {
   const category = event.params.category as Categories;
+  const keys = Object.keys(json);
+  if (!keys.includes(category)) {
+    throw error(404, "Page not found");
+  }
   const categories = Object.values(json).map(({ href, label }) => ({
     href,
     label,
