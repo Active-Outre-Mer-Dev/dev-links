262bef1e3a1710206181083083db081d2a4cb717
bluepnwage <a.carty2555@gmail.com>
2023-04-08 11:49:10 -0400

    create theme toggle

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { onMount } from "svelte";
+  import ThemeToggle from "./ThemeToggle.svelte";
 
   let AlgoliaSearch: any;
 
@@ -40,35 +41,38 @@
     <div>
       <img src="/aom.svg" alt="" width={65} height={65} />
     </div>
-    <button
-      on:click={onOpen}
-      class={`rounded-full h-3/4 px-8 gap-8  flex items-center justify-between
-       bg-neutral-900 border border-neutral-700`}
-    >
-      <div class="space-x-2 text-neutral-400">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="inline-block"
-          width="16"
-          height="16"
-          viewBox="0 0 24 24"
-          stroke-width="2"
-          stroke="currentColor"
-          fill="none"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        >
-          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-          <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
-          <path d="M21 21l-6 -6" />
-        </svg>
-        <span> Search </span>
-      </div>
-      <div class="text-neutral-300">
-        <kbd class="bg-neutral-800 p-1 w-5 h-5 text-xs rounded-sm uppercase">Ctrl</kbd>
-        <kbd class="bg-neutral-800 p-1 w-5 h-5 text-xs rounded-sm">K</kbd>
-      </div>
-    </button>
+    <div class="flex items-center justify-center h-full gap-4">
+      <button
+        on:click={onOpen}
+        class={`rounded-full  h-3/4 px-8 gap-8  flex items-center justify-between
+         bg-neutral-900 border border-neutral-700`}
+      >
+        <div class="space-x-2 text-neutral-400">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="inline-block"
+            width="16"
+            height="16"
+            viewBox="0 0 24 24"
+            stroke-width="2"
+            stroke="currentColor"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
+            <path d="M21 21l-6 -6" />
+          </svg>
+          <span> Search </span>
+        </div>
+        <div class="text-neutral-300">
+          <kbd class="bg-neutral-800 p-1 w-5 h-5 text-xs rounded-sm uppercase">Ctrl</kbd>
+          <kbd class="bg-neutral-800 p-1 w-5 h-5 text-xs rounded-sm">K</kbd>
+        </div>
+      </button>
+      <ThemeToggle />
+    </div>
   </div>
 </header>
 {#if showSearch}
