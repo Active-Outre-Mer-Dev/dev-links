39faab275f7db4506b99af3f2c1bc2aacf3c8e02
bluepnwage <a.carty2555@gmail.com>
2023-04-14 13:57:59 -0400

    use isr

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,6 +1,9 @@
 import { getCategories } from "$lib/notion";
 // since there's no dynamic data here, we can prerender
 // it so that it gets served as a static asset in production
+
+export const prerender = true;
+
 export const load = async () => {
   const links = await getCategories();
   return {
